0ba7fd3109ed48f6e0e073639f64eb609781acac
Postescu Cristian <postescu@gmail.com>
2020-08-15 19:17:33 +0300

    post heading share icons

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -10,6 +10,13 @@ import Layout from '../components/layout';
 import BlogPostItem from '../components/shared/blog-post-item';
 import mdxComponents from '../components/mdxComponents';
 import PostPlaceholder from '../components/post-placeholder';
+import github from '../images/footer/github-icon.svg';
+import slack from '../images/footer/slack-icon.svg';
+import youtube from '../images/footer/youtube-icon.svg';
+import twitter from '../images/footer/twitter-icon.svg';
+import linkedIn from '../images/footer/linkedin-icon.svg';
+import { docsRedirect } from '../components/shared/tracking-utils';
+import docs from '../images/footer/docs-icon.svg';
 
 const BlogPostTemplate = ({ data }) => {
   const post = data.mdx;
@@ -29,6 +36,48 @@ const BlogPostTemplate = ({ data }) => {
           <div className={`${styles.blogPostContainer} container`}>
             <div className='row'>
               <div className='col-12'>
+                <div className={styles.postCategory}>{post.frontmatter.category}</div>
+                <div className={styles.postShare}>
+                  <div>
+                    <a
+                      href='https://work.withpixie.ai/docs'
+                      onClick={(e) => docsRedirect(e)}
+                    >
+                      <img src={docs} className={styles.socialIcon} />
+                    </a>
+                    <a href='https://github.com/pixie-labs/pixie'>
+                      <img src={github} className={styles.socialIcon} />
+                    </a>
+                    <a href='https://slackin.withpixie.ai'>
+                      <img src={slack} className={styles.socialIcon} />
+                    </a>
+                    <a href='https://www.youtube.com/channel/UCOMCDRvBVNIS0lCyOmst7eg/featured'>
+                      <img src={youtube} className={styles.socialIcon} />
+                    </a>
+                    <a href='https://twitter.com/pixie_run'>
+                      <img src={twitter} className={styles.socialIcon} />
+                    </a>
+                    <a href='https://www.linkedin.com/company/pixieai/'>
+                      <img src={linkedIn} className={styles.socialIcon} />
+                    </a>
+                  </div>
+                  <div>
+                    <div>
+                      {post.frontmatter.author}
+                    </div>
+                    <div className={styles.postDate}>
+                      {post.frontmatter.date}
+                      {' • '}
+                      {post.timeToRead}
+                      {' '}
+                      minute
+                      {post.timeToRead > 1 ? 's' : ''}
+                      {' '}
+                      read
+                    </div>
+
+                  </div>
+                </div>
                 <h1>{post.frontmatter.title}</h1>
                 <p>{post.frontmatter.subtitle || post.excerpt}</p>
                 <span>{post.frontmatter.date}</span>
@@ -70,10 +119,14 @@ export const pageQuery = graphql`
     mdx(fields: { slug: { eq: $slug } }) {
       excerpt(pruneLength: 160)
       body
+              timeToRead
+
       frontmatter {
         title
         subtitle
         date
+        category
+        author
         featured_image {
           childImageSharp {
             fluid(maxWidth: 1200, quality: 92) {
