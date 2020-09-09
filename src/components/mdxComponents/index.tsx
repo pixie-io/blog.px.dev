87d5d3f795bf4bfd6b934ab473192752969b7bf1
Postescu Cristian <postescu@gmail.com>
2020-09-09 19:58:02 +0300

    quote component

--- src/components/mdxComponents/index.tsx
+++ src/components/mdxComponents/index.tsx
@@ -3,9 +3,11 @@ import React from 'react';
 import baseComponents from './baseComponents';
 import SvgRenderer from './svg';
 import Command from './command';
+import Quote from './quote';
 
 export default {
   ...baseComponents,
   svg: (props: any) => <SvgRenderer {...props} />,
+  quote: (props: any) => <Quote {...props} />,
   command: (props: any) => <Command {...props} />,
 };
---
+++ src/components/mdxComponents/quote.tsx
@@ -0,0 +1,25 @@
+import withStyles from '@material-ui/core/styles/withStyles';
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+
+const Quote = withStyles(() => ({
+  quote: {
+    padding: '17px',
+    borderStyle: 'solid',
+    border: '0',
+    borderLeft: '5px',
+    borderColor: '#12D6D6',
+    backgroundColor: '#353535',
+    borderRadius: '0 5px 5px 0',
+    marginBottom: '32px',
+    marginTop: '32px',
+    fontSize: '18px',
+    lineHeight: '30px',
+  },
+
+}))((props) => {
+  const { children, classes } = props;
+  return (<div className={classes.quote}>{children}</div>);
+});
+
+export default Quote;
