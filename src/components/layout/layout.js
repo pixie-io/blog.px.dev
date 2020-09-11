a214781f7761543d9dbfc019fd960b1cc55e9420
Postescu Cristian <postescu@gmail.com>
2020-09-11 16:29:34 +0300

    lint

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 import Header from '../header';
 import Footer from '../footer';
-import { ThemeModeContext } from '../mainThemeProvider';
+import { ThemeModeContext } from '../mainThemeProvider.tsx';
 
 const Layout = ({
   children, whiteHeader, whiteFooter, hideMenu, showSwitch,
