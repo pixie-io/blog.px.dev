efbcae8f913d2b41a2cd68df2447bffda4cde6b3
Postescu Cristian <postescu@gmail.com>
2020-09-14 21:51:53 +0300

    Weird artifact in the bottom of the code snippets in light mode (http://localhost:8000/blogdata/blog/pixie-tutorial-1/pixie-tutorial-1/)

--- src/components/mdxComponents/codeRenderer.tsx
+++ src/components/mdxComponents/codeRenderer.tsx
@@ -24,7 +24,7 @@ const CodeRenderer = withStyles((theme) => ({
   code: {
     backgroundColor: theme.palette.type === 'light' ? '#f3f3f3' : '#292929',
     borderRadius: '5px',
-    boxShadow: '0px 4px 16px rgba(0, 0, 0, 0.15)',
+    boxShadow: theme.palette.type === 'light' ? '0 2px 2px rgba(0,0,0, 0.15)' : '0px 4px 16px rgba(0, 0, 0, 0.15)',
     marginBottom: '32px',
     marginTop: '12px',
     position: 'relative',
