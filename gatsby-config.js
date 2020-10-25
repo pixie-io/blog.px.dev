f0ca34b6ac1fca5c498bb397cf4e01472d236a9d
Postescu Cristian <postescu@gmail.com>
2020-10-25 23:41:48 +0200

    Fix URL in the blog page

--- gatsby-config.js
+++ gatsby-config.js
@@ -131,6 +131,7 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    'gatsby-plugin-netlify',
   ],
   mapping: {
     'Mdx.frontmatter.author': 'authorYaml',
